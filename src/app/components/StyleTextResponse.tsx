729788be6444eb182c76daf1c1c1bcfd0fa3f02a
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-10-29 16:27:25 -0600

    Start on some initial styles and layout

--- src/app/components/StyleTextResponse.tsx
+++ src/app/components/StyleTextResponse.tsx
@@ -1,5 +1,7 @@
 const StyleTextResponse = ({ text }: { text: string }) => {
-  return <p>{text}</p>;
+  return (
+    <p className="text-body-primary font-semibold text-body mt-6">{text}</p>
+  );
 };
 
 export { StyleTextResponse };
