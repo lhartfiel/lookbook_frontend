5b9c8aa8001ab0fcd831ee99a830d9483e547209
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-11-03 20:33:36 -0700

    Start on dark/light toggle

---
+++ src/app/components/Common/ToggleMode.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faToggleOff, faToggleOn } from "@fortawesome/free-solid-svg-icons";
+
+const ToggleMode = ({
+  handleToggle,
+  showDarkMode,
+}: {
+  handleToggle: () => void;
+  showDarkMode: boolean;
+}) => {
+  return (
+    <>
+      {showDarkMode ? (
+        <FontAwesomeIcon
+          onClick={handleToggle}
+          className="text-4xl"
+          icon={faToggleOff}
+        ></FontAwesomeIcon>
+      ) : (
+        <FontAwesomeIcon
+          onClick={handleToggle}
+          className="text-4xl"
+          icon={faToggleOn}
+        ></FontAwesomeIcon>
+      )}
+    </>
+  );
+};
+
+export { ToggleMode };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 import { Chatbot } from "./components/Chatbot";
+import { ToggleMode } from "./components/Common/ToggleMode";
 
 export default function Home() {
+  const [showDarkMode, setShowDarkMode] = useState(false);
+  const handleToggle = () => {
+    setShowDarkMode((prev) => !prev);
+  };
+
   return (
     <div className="grid grid-cols-4 lg:grid-cols-12 lg:gap-4 max-w-[1440px] mx-auto px-6 py-6">
       <div className="col-span-full lg:col-span-8 lg:col-start-3">
@@ -14,7 +22,18 @@ export default function Home() {
             sizes="(max-width: 768px) 100vw, 33vw"
           />
         </div>
+
         <Chatbot></Chatbot>
+      </div>
+      <div className="col-span-3 lg:col-start-11 text-primary justify-start align-start">
+        <span className="flex flex-nowrap items-center">
+          <p className="mr-2 font-bold">Dark Mode</p>
+          <ToggleMode
+            handleToggle={handleToggle}
+            showDarkMode={showDarkMode}
+          ></ToggleMode>
+          <p className="ml-2 font-bold">Light Mode</p>
+        </span>
       </div>
     </div>
   );
