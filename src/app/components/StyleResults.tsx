8aa24541a0c73cf7bef9709561d660377b70fe65
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-11-07 11:33:29 -0700

    Remove unused useCallback in Results

--- src/app/components/StyleResults.tsx
+++ src/app/components/StyleResults.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { StyleGallery } from "./StyleGallery";
